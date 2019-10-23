fde62770de9acf29341b1e090673196e1bbacd0a
Pauli Jokela <pauli.jokela@didstopia.com>
2019-10-23 09:12:50 +0300

    Pushing changes

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "github.com/reiver/go-telnet"
+import (
+	"log"
+
+	"github.com/reiver/go-telnet"
+)
 
 func main() {
 	var caller telnet.Caller = telnet.StandardCaller
 
 	//@TOOD: replace "example.net:5555" with address you want to connect to.
+	log.Println("Connecting to telnet server..")
 	telnet.DialToAndCall("example.net:5555", caller)
 }
